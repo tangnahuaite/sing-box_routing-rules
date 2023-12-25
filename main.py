50b25a1e962670c156bf6cef2465c7f491aa9768
tangnahuaite <75670474+tangnahuaite@users.noreply.github.com>
2023-12-24 17:58:54 -0800

    Update main.py

--- main.py
+++ main.py
@@ -42,6 +42,8 @@ def parse_and_convert_to_dataframe(link):
                         if address.startswith('+'):
                             pattern = 'DOMAIN-SUFFIX'
                             address = address[1:]
+                            if address.startswith('.'):
+                                address = address[2:]
                         else:
                             pattern = 'DOMAIN'
                 else:
@@ -83,10 +85,22 @@ def parse_list_file(link, output_directory):
     os.makedirs(output_directory, exist_ok=True)
 
     result_rules = {"version": 1, "rules": []}
+    domain_entries = []
 
     for pattern, addresses in df.groupby('pattern')['address'].apply(list).to_dict().items():
-        rule_entry = {pattern: [address.strip() for address in addresses]}
-        result_rules["rules"].append(rule_entry)
+        if pattern == 'domain_suffix':
+            rule_entry = {pattern: ['.' + address.strip() for address in addresses]}
+            result_rules["rules"].append(rule_entry)
+            domain_entries.extend([address.strip() for address in addresses])
+        elif pattern == 'domain':
+            domain_entries.extend([address.strip() for address in addresses])
+        else:
+            rule_entry = {pattern: [address.strip() for address in addresses]}
+            result_rules["rules"].append(rule_entry)
+    # 删除 'domain_entries' 中的重复值
+    domain_entries = list(set(domain_entries))
+    if domain_entries:
+        result_rules["rules"].insert(0, {'domain': domain_entries})
 
     # 使用 output_directory 拼接完整路径
     file_name = os.path.join(output_directory, f"{os.path.basename(link).split('.')[0]}.json")
